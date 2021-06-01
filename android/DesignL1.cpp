e5814d9c8c8e42472f6c8c3afff705e176ea60fb
Husain <sajorawala@gmail.com>
2021-06-02 04:32:55 +0930

    Initial commit on Level1 branch

---
+++ android/DesignL1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hussain Sajorawala on 2/06/2021.
+//
+
+#include "DesignL1.h"
---
+++ android/DesignL1.h
@@ -0,0 +1,16 @@
+//
+// Created by Hussain Sajorawala on 2/06/2021.
+//
+
+#ifndef AGD_GROUP_ASSIGNMENT_DESIGNL1_H
+#define AGD_GROUP_ASSIGNMENT_DESIGNL1_H
+
+
+
+class DesignL1 {
+
+};
+
+
+
+#endif //AGD_GROUP_ASSIGNMENT_DESIGNL1_H
