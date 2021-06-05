e52ccdee971b57398551b802e1ba79891575adc0
DinujaWickrama <wildy039@mymail.unisa.edu.au>
2021-06-05 14:18:29 +0930

    Create Menu class for Main Menu and design main menu.

---
+++ core/src/com/mygdx/game/Menu.java
@@ -0,0 +1,169 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class Menu implements Screen
+{
+    MyGdxGame game;
+    private Texture background;
+    private Texture menuBG;
+    private SpriteBatch batch;
+    private Stage stage;
+    private Skin skin;
+
+    Image imgBackground;
+    Image menuBackground;
+    //Animation<TextureRegion> animation;
+    float elapsed;
+    TextButton btnPlay;
+    Sound btnSound;
+    TextButton btnOption;
+    TextButton btnExit;
+
+    public Menu(MyGdxGame game)
+    {
+        this.game = game;
+    }
+    public void create()
+    {
+        batch = new SpriteBatch();
+        stage = new Stage();
+        menuBG = new Texture("Starting Assets/assets/finishedbg.png");
+        background = new Texture("Starting Assets/assets/menuBackGround.gif");
+        skin = new Skin(Gdx.files.internal("Starting Assets/assets/uiskin.json"));
+        btnPlay = new TextButton("Play", skin, "default");
+        btnOption = new TextButton("Option",skin,"default");
+        btnExit = new TextButton("Exit", skin, "default");
+        btnSound = Gdx.audio.newSound(Gdx.files.internal("Starting Assets/assets/buttonsound.wav"));
+        //animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("Starting Assets/assets/menuBackGround.gif").read());
+
+        imgBackground = new Image(background);
+        imgBackground.setSize(2300,1100);
+        imgBackground.setZIndex(1);
+
+
+        menuBackground = new Image(menuBG);
+        menuBackground.setSize(1800,900);
+        menuBackground.setX(200);
+        menuBackground.setY(200);
+        menuBackground.setZIndex(2);
+
+
+        btnPlay.setWidth(600f);
+        btnPlay.setHeight(100f);
+        btnPlay.getLabel().setFontScale(5);
+        btnPlay.setColor(Color.GOLD);
+        btnPlay.setPosition(750, 800);
+
+        btnOption.setWidth(600f);
+        btnOption.setHeight(100f);
+        btnOption.getLabel().setFontScale(5);
+        btnOption.setColor(Color.GOLD);
+        btnOption.setPosition(750, 600);
+
+        btnExit.setWidth(600f);
+        btnExit.setHeight(100f);
+        btnExit.getLabel().setFontScale(5);
+        btnExit.setColor(Color.GOLD);
+        btnExit.setPosition(750, 400);
+
+
+        btnPlay.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked (InputEvent event, float x, float y)
+            {
+                btnSound.play(1.0f);
+
+            }
+        });
+
+        btnExit.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked (InputEvent event, float x, float y)
+            {
+                Gdx.app.exit();
+                btnSound.play(1.0f);
+
+            }
+        });
+
+        btnOption.addListener(new ClickListener(){
+            @Override
+            public void clicked (InputEvent event, float x, float y)
+            {
+                btnSound.play(1.0f);
+
+            }
+        });
+        //stage.addActor(animActor);
+        stage.addActor(imgBackground);
+        stage.addActor(menuBackground);
+        stage.addActor(btnPlay);
+        stage.addActor(btnExit);
+        stage.addActor(btnOption);
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show()
+    {
+        Gdx.app.log("MenuScreen: ","MenuScreen Show Called");
+        create();
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        skin.dispose();
+        menuBG.dispose();
+        btnSound.dispose();
+    }
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,33 @@
 package com.mygdx.game;
-
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game implements ApplicationListener
+{
 	SpriteBatch batch;
 	Texture img;
-	
+	public static Menu menu;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create ()
+	{
+		Gdx.app.log("MyGdxGame: "," Create");
+		menu = new Menu(this);
+		setScreen(menu);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render ()
+	{
+		super.render();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose ()
+	{
+		super.dispose();
 	}
 }
