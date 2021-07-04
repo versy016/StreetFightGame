497ea8be2ca59c8cc3ca70a756a2763f9a1b8b60
shivam verma <versy016@mymail.unisa.edu.au>
2021-07-05 05:01:39 +0930

    updated project

---
+++ android/assets/China_Town_Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="China_Town_Map" tilewidth="30" tileheight="30" tilecount="275" columns="25">
+ <image source="China_Town_Map.png" width="750" height="352"/>
+</tileset>
